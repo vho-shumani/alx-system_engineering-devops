f1137b3122d0d62cfc7879ca9f2dbc2deedb9f3d
Shumani Simeli <shumanisimeli@gmail.com>
2024-03-22 20:17:27 +0200

    created 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,6 @@
 #install flask from pip3.
 
-class { 'python': }
-
 package { 'flask':
-  ensure          => installed,
-  provider        => 'pip3',
-  require         => Class['python'],
-  install_options => [ { '--version' => '2.1.0' } ],
+  ensure   => '2.1.0'
+  provider => 'pip3',
 }
