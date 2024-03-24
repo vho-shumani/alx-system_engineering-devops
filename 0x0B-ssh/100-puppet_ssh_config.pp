2a4760c68c963c8d5c303214a68fab6ee78bc723
Shumani Simeli <shumanisimeli@gmail.com>
2024-03-24 21:26:37 +0200

    created 100-puppet_ssh_config.pp

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,16 @@
+# using Puppet to make changes to our configuration file.
+file { '/etc/ssh/ssh_config':
+  ensure => present,
+}
+
+file_line { 'Turn off passwd auth':
+  path  => '/etc/ssh/ssh_config',
+  line  => 'PasswordAuthentication no',
+  match => '^#PasswordAuthentication',
+}
+
+file_line { 'Declare identity file':
+  path  => '/etc/ssh/ssh_config',
+  line  => 'IdentityFile  ~/.ssh/school',
+  match => '^#IdentityFile',
+}
