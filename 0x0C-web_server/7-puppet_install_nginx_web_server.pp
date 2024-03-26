4b2f61deb8fdf98868eb1102446d1f6ebbf145af
Shumani Simeli <shumanisimeli@gmail.com>
2024-03-27 00:44:06 +0200

    updated 7-puppet_install_nginx_web_server.pp
    mend

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,50 +1,23 @@
 #install and configures nginx
-class nginx {
-
-  include puppet::nginx
-
-  package { 'nginx':
-    ensure => installed,
-    require => Exec['update system'],
-  }
-
-  Exec['update system'] {
-    command => '/usr/bin/apt-get update',
-  }
-
-  nginx::vhost { 'default':
-    ensure        => present,
-    listen_port   => 80,
-    server_name   => ['localhost'],
-    www_root      => '/var/www/html',
-    index_files   => ['index.html'],
-    content => <<EOF
-<!DOCTYPE html>
-<html>
-<head>
-<title>Hello World!</title>
-</head>
-<body>
-<h1>Hello World!</h1>
-</body>
-</html>
-EOF
-  }
-
-  nginx::resource { 'redirect':
-    location => '/redirect_me',
-    rewrite  => {
-      source   => '^/redirect_me$',
-      destination => 'https://www.youtube.com/watch?v=QH2-TGUlwu4',
-      permanent => true,
-    }
-    require => Package['nginx']
-  }
-
-  service { 'nginx':
-    ensure => running,
-    enable => true,
-    require => Package['nginx'],
-  }
+{ 'update system':
+        command => '/usr/bin/apt-get update',
 }
 
+package { 'nginx':
+	ensure => 'installed',
+	require => Exec['update system']
+}
+
+file {'/var/www/html/index.html':
+	content => 'Hello World!'
+}
+
+exec {'redirect_me':
+	command => 'sed -i "24i\	rewrite ^/redirect_me https://www.google permanent;" /etc/nginx/sites-available/default',
+	provider => 'shell'
+}
+
+service {'nginx':
+	ensure => running,
+	require => Package['nginx']
+}
