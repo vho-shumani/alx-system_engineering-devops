fdf83f6bcf9e2a6d729cdabe8c30777e15ca47c7
Shumani Simeli <shumanisimeli@gmail.com>
2024-03-27 00:20:22 +0200

    created 2-setup_a_domain_name 7-puppet_install_nginx_web_server.pp

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,27 @@
+exec { 'update system':
+        command => '/usr/bin/apt-get update',
+}
+
+package { 'nginx':
+	ensure => 'installed',
+	require => Exec['update system']
+}
+
+file {'/var/www/html/index.html':
+	content => 'Hello World!'
+}
+
+nginx::resource { 'redirect':
+    location => '/redirect_me',
+    rewrite  => {
+      source   => '^/redirect_me$',
+      destination => 'https://www.youtube.com/watch?v=QH2-TGUlwu4',
+      permanent => true,
+    }
+    require => Package['nginx']
+  }
+
+service {'nginx':
+	ensure => running,
+	require => Package['nginx']
+}
