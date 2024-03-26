86381e327e80ecfcc0aad2d050342bf9c91b011e
Shumani Simeli <shumanisimeli@gmail.com>
2024-03-27 00:39:28 +0200

    updated 7-puppet_install_nginx_web_server.pp
    mend

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,28 +1,50 @@
-# Install and configure an nginx server
-exec { 'update system':
-        command => '/usr/bin/apt-get update',
-}
+#install and configures nginx
+class nginx {
 
-package { 'nginx':
-  ensure  => 'installed',
-  require => Exec['update system']
-}
+  include puppet::nginx
 
-file {'/var/www/html/index.html':
-  content => 'Hello World!'
-}
-
-nginx::resource { 'redirect':
-    location => '/redirect_me',
-    rewrite  => {
-      source      => '^/redirect_me$',
-      destination => 'https://www.youtube.com/watch?v=QH2-TGUlwu4',
-      permanent   => true,
-    }
-    require  => Package['nginx']
+  package { 'nginx':
+    ensure => installed,
+    require => Exec['update system'],
   }
 
-service {'nginx':
-  ensure  => running,
-  require => Package['nginx']
+  Exec['update system'] {
+    command => '/usr/bin/apt-get update',
+  }
+
+  nginx::vhost { 'default':
+    ensure        => present,
+    listen_port   => 80,
+    server_name   => ['localhost'],
+    www_root      => '/var/www/html',
+    index_files   => ['index.html'],
+    content => <<EOF
+<!DOCTYPE html>
+<html>
+<head>
+<title>Hello World!</title>
+</head>
+<body>
+<h1>Hello World!</h1>
+</body>
+</html>
+EOF
+  }
+
+  nginx::resource { 'redirect':
+    location => '/redirect_me',
+    rewrite  => {
+      source   => '^/redirect_me$',
+      destination => 'https://www.youtube.com/watch?v=QH2-TGUlwu4',
+      permanent => true,
+    }
+    require => Package['nginx']
+  }
+
+  service { 'nginx':
+    ensure => running,
+    enable => true,
+    require => Package['nginx'],
+  }
 }
+
