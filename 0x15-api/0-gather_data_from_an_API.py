49c1b2ff5df58977c96fc4fc25f4aef58f3f4086
Shumani <shumanisimeli@gmail.com>
2024-04-22 23:38:49 +0200

    first commit

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Using the REST API, for a given employee ID, returns information about his/her TODO list progress."""
+import requests
+import json
+import sys
+
+if __name__ == "__main__":
+    employee_id = sys.argv[1]
+    employee_detail = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}")
+    todo_list = requests.get(f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}")
+    completed_tasks = len([todo for todo in todo_list.json() if todo['completed'] == True])
+    number_of_tasks = len(todo_list.json())
+    
+    print(f'Employee {employee_detail.json()["name"]} is done with tasks({completed_tasks}/{number_of_tasks}):')
+
+    for todo in todo_list.json():
+        if todo['completed'] == True:
+            print(f"\t {todo['title']}")
