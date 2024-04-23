d7abd0c12440a5313c9b1607f4ff62422a11fd97
Shumani <shumanisimeli@gmail.com>
2024-04-23 02:40:30 +0200

    created 3-dictionary_of_list_of_dictionaries.py

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""using the REST API, for a given employee ID,
+returns information about his/her TODO list progress."""
+import csv
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+    employees_detail = requests.get(
+            f"https://jsonplaceholder.typicode.com/users"
+            )
+    todo_list = requests.get(
+            f"https://jsonplaceholder.typicode.com/todos"
+            )
+    tasks = {}
+    for employee in employees_detail.json():
+        value = [
+                {
+                    "username": employee['username'],
+                    "task": todo['title'],
+                    "completed": todo['completed'],
+                } for todo in todo_list.json() if todo['userId'] == employee['id']
+                ]
+        tasks[employee['id']] = value
+    with open("todo_all_employees.json", 'w') as f:
+        json.dump(tasks, f)
