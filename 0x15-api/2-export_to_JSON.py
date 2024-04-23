213ae3226ac635601a751140a8c8ab1319a9bfb4
Shumani <shumanisimeli@gmail.com>
2024-04-23 02:05:10 +0200

    created 2-export_to_JSON.py

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""using the REST API, for a given employee ID,
+returns information about his/her TODO list progress."""
+import csv
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+    employee_id = sys.argv[1]
+    employee_detail = requests.get(
+            f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+            )
+    todo_list = requests.get(
+            f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
+            )
+    completed_tasks = len([x for x in todo_list.json() if x['completed']])
+    number_of_tasks = len(todo_list.json())
+
+#    print(f'Employee {employee_detail.json()["name"]} is', end='')
+#    print(f' done with tasks({completed_tasks}/{number_of_tasks}):')
+#    for todo in todo_list.json():
+#        if todo['completed']:
+#            print(f"\t {todo['title']}")
+    value = [
+            {
+                "task": todo['title'],
+                "completed": todo['completed'],
+                "username": employee_detail.json()['username']
+                } for todo in todo_list.json()
+            ]
+    with open(f"{employee_id}.json", 'w') as f:
+        json.dump({employee_id: value}, f)
