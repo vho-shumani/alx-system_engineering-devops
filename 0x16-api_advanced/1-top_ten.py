b25d79769332b0f270a567d9c15d7d400b0c1cec
Shumani Simeli <shumanisimeli@gmail.com>
2024-05-08 00:01:28 +0200

    created 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,13 +6,10 @@ import requests
 
 def top_ten(subreddit):
     title_list = []
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=9'
     response = requests.get(url, allow_redirects=False)
-    i = 0
     if response.status_code == 200:
         for data in response.json()['data']['children']:
-            if i < 10:
                 print(data['data']['title'])
-            i += 1
     elif response.status_code == 404:
         print(None)
