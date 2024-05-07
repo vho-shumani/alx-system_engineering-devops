81273ea7a8cb27c78fbcaaf799b623e9120b7fea
Shumani Simeli <shumanisimeli@gmail.com>
2024-05-08 01:11:05 +0200

    update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,13 +4,12 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    try:
-        url = f'https://www.reddit.com/r/{subreddit}/about.json'
-        response = requests.get(url, allow_redirects=False)
-        if response.status_code == 200:
-            subscribers = response.json()['data']['subscribers']
-            return subscribers
-        else:
-            return 0
-    except Exception as e:
+    """Return the total number of subscribers on a given subreddit."""
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        subscribers = response.json()['data']['subscribers']
+        return subscribers
+    else:
         return 0
