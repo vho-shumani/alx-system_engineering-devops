9964671f4e3372b42cd05d3d1d7a30c201f7a57b
Shumani Simeli <shumanisimeli@gmail.com>
2024-05-08 01:22:03 +0200

    updated 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,7 @@ def number_of_subscribers(subreddit):
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
+    if response.status_code < 300:
         subscribers = response.json()['data']['subscribers']
         return subscribers
     else:
