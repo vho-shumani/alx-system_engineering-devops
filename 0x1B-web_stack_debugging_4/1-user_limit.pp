77603e1541246b489f57dab3cd1d7efa3b065783
Shumani Simeli <shumanisimeli@gmail.com>
2024-05-17 00:29:04 +0200

    updated 1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,9 +1,10 @@
 #Change the OS configuration so that it is possible to login with the holberton user and open a file without any error message.
 
-exec { 'increase-file-limits-for-holberton-user':
-command => [
-'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
-'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
-],
-path => '/usr/local/bin/:/bin/',
+exec { 'increase-hard-file':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+exec { 'increase-soft-file':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
 }
